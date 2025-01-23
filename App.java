f85107c0dc5f71cd7ea377d28a93d76f974e93e2
fromerog03 <fernandoromero>
2025-01-23 18:20:59 +0100

    NUEVO CAMBIO A APP.JAVA
    CANCIÓN CONTINUACIÓN

--- App.java
+++ App.java
@@ -1,6 +1,7 @@
 public class App{
     public static void main(String[] args) {
         System.out.println("Arranca la aplicación");
+        System.out.println("cafe con nata color mulata ellas es activa y su cuerpo un poema llega con salta la serenata");
     }
 
 }
